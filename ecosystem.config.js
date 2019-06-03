dbbbe1478659a5318b35ed1069a8828c7dfa3992
Boris Ryabov <rv.boris@gmail.com>
2019-06-04 02:43:57 +0300

    WIP

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       script: 'dist/bot.js',
       instances: 1,
       autorestart: true,
-      watch: true,
+      watch: false,
       max_memory_restart: '1G',
       env: {
         NODE_ENV: 'development',
