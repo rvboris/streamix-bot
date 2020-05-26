e8c771a87b18843293983cf6cdeb6185edaab4d7
Boris Ryabov <rv.boris@gmail.com>
2020-05-27 02:00:10 +0300

    ci: revert pm2 env settings

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,6 +7,12 @@ module.exports = {
       autorestart: true,
       watch: false,
       max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
     },
   ],
 };
